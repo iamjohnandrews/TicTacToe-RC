a4a15b61b1431cb8fc6ce51b84eff4fe94fef654
iamjohnandrews <johnnydrews@gmail.com>
2015-09-01 08:13:03 -0400

    add player scores to label

--- TTTViewController.m
+++ TTTViewController.m
@@ -7,14 +7,19 @@
 //
 
 #import "TTTViewController.h"
-#import "Player.h"
 #import "TTTCollectionViewCell.h"
+
+NSString * const playerOneScoreKey = @"playerOneVictoryCount";
+NSString * const playerTwoScoreKey = @"playerTwoVictoryCount";
 
 @interface TTTViewController () <UIAlertViewDelegate>
 @property (nonatomic) BOOL isFirstPlayersTurn;
 @property (nonatomic, strong) NSString *winner;
 @property (nonatomic, strong) NSMutableArray *gameBoardPlacesX;
 @property (nonatomic, strong) NSMutableArray *gameBoardPlacesO;
+@property (nonatomic, strong) NSNumber *playerOneVictories;
+@property (nonatomic, strong) NSNumber *playerTwoVictories;
+
 @end
 
 @implementation TTTViewController
@@ -22,12 +27,37 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self startNewGame];
+    [self getPlayersScores];
+}
+
+- (void)viewDidDisappear:(BOOL)animated {
+    [super viewDidDisappear:animated];
+    [[NSUserDefaults standardUserDefaults]setObject:self.playerOneVictories forKey:playerOneScoreKey];
+    [[NSUserDefaults standardUserDefaults]setObject:self.playerTwoVictories forKey:playerTwoScoreKey];
+    [[NSUserDefaults standardUserDefaults] synchronize];
 }
 
 - (void)startNewGame {
     self.isFirstPlayersTurn = YES;
     self.gameBoardPlacesO = [NSMutableArray array];
     self.gameBoardPlacesX = [NSMutableArray array];
+}
+
+- (void)getPlayersScores {
+    self.playerOneVictories = [[NSUserDefaults standardUserDefaults] objectForKey:playerOneScoreKey];
+    self.playerTwoVictories = [[NSUserDefaults standardUserDefaults] objectForKey:playerTwoScoreKey];
+    
+    if (self.playerOneVictories) {
+        self.playerOneScore.text = [NSString stringWithFormat:@"%@", self.playerOneVictories];
+    } else {
+        self.playerOneScore.text = @"0";
+    }
+    
+    if (self.playerTwoVictories) {
+        self.playerTwoScore.text = [NSString stringWithFormat:@"%@", self.playerTwoVictories];
+    } else {
+        self.playerTwoScore.text = @"0";
+    }
 }
 
 #pragma mark DataSource Methods
@@ -89,8 +119,10 @@
     if (winner) {
         if (self.isFirstPlayersTurn) {
             winningPlayer = @"Player 2";
+            self.playerTwoVictories = [NSNumber numberWithInt:[self.playerTwoVictories intValue] + 1];
         } else {
             winningPlayer = @"Player 1";
+            self.playerOneVictories = [NSNumber numberWithInt:[self.playerOneVictories intValue] + 1];
         }
         NSLog(@"player %@ WON", winner);
         UIAlertView *alert = [[UIAlertView alloc]initWithTitle:@"WINNER"
@@ -116,7 +148,10 @@
 #pragma mark - Game Logic Methods
 
 - (void)isThereAWinner {
-    
+    if (self.gameBoardPlacesX.count + self.gameBoardPlacesO.count < 5) {
+        return;
+    }
+    /*
     if ([self isThreeInRowDiagonal:self.gameBoardPlacesX]) {
         self.winner = @"X";
     } else if ([self isThreeInRowHorizontal:self.gameBoardPlacesX]) {
@@ -132,6 +167,66 @@
     } else if ([self isThreeInRowVertical:self.gameBoardPlacesO]) {
         self.winner = @"O";
     }
+    */
+    
+    /* works but ugly -- move these calls to a method that loops through em all
+    if ([self isThreeInARowFrom:self.gameBoardPlacesX with:0 to:3 incrementCellsBy:1] ||
+        [self isThreeInARowFrom:self.gameBoardPlacesX with:3 to:6 incrementCellsBy:1] ||
+        [self isThreeInARowFrom:self.gameBoardPlacesX with:6 to:9 incrementCellsBy:1]) {
+        self.winner = @"X";
+    }  else if ([self isThreeInARowFrom:self.gameBoardPlacesX with:2 to:7 incrementCellsBy:2] ||
+                [self isThreeInARowFrom:self.gameBoardPlacesX with:0 to:9 incrementCellsBy:4]) {
+        self.winner = @"X";
+    } else if ([self isThreeInARowFrom:self.gameBoardPlacesX with:0 to:7 incrementCellsBy:3] ||
+               [self isThreeInARowFrom:self.gameBoardPlacesX with:1 to:8 incrementCellsBy:3] ||
+               [self isThreeInARowFrom:self.gameBoardPlacesX with:2 to:9 incrementCellsBy:3]) {
+        self.winner = @"X";
+    }
+    */
+
+    NSNumber *max=[self.gameBoardPlacesX valueForKeyPath:@"@max.self"];
+    NSNumber *min=[self.gameBoardPlacesX valueForKeyPath:@"@min.self"];
+    
+    if ([self isThreeInARowFrom:self.gameBoardPlacesX with:[min integerValue] to:[max integerValue] incrementCellsBy:1]) {
+        self.winner = @"X";
+    }
+
+}
+
+/* works but ugly
+- (BOOL)isThreeInARowFrom:(NSArray *)playerMoves with:(int)startCell to:(int)endCell incrementCellsBy:(int)increment {
+    BOOL doHaveAWinner = YES;
+    for  (int move = startCell; move < endCell; move+= increment) {
+        if (![playerMoves containsObject:[NSNumber numberWithInt:move]]) {
+            doHaveAWinner = NO;
+        }
+    }
+
+    return doHaveAWinner;
+}
+*/
+
+
+
+- (BOOL)isThreeInARowFrom:(NSArray *)playerMoves with:(NSInteger)startCell to:(NSInteger)endCell incrementCellsBy:(int)increment {
+    BOOL doHaveAWinner = YES;
+    int counter;
+    for  (NSInteger move = 0; move < 9; move+= increment) {
+        counter++;
+        NSLog(@"counter =%d", counter);
+        if (![playerMoves containsObject:[NSNumber numberWithInteger:move]]) {
+            doHaveAWinner = NO;
+        }
+        if (counter % 3 == 0) {
+            if (doHaveAWinner) {
+                break;
+            } else {
+                doHaveAWinner = YES;
+            }
+        }
+    }
+    
+    return doHaveAWinner;
 }
 
 -(BOOL)isThreeInRowHorizontal:(NSArray *)playerMoves {
